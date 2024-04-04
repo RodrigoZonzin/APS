21564b745e61b2787734ce83a9ca5ebbbcf919a5
RodrigoZonzin <rodrigojzonzin@gmail.com>
2024-04-04 11:02:20 -0300

    indo

--- view/JanelaPrincipal.py
+++ view/JanelaPrincipal.py
@@ -11,19 +11,29 @@ class Janela:
         toplevel.configure(bg="#6cbd74")
 
 
-
+        #CONTAINER QUE CONTERÁ A BARRA DE PESQUISA E O BOTÃO DE LOGIN
         self.fr1 = Frame(toplevel)
-        self.fr1.pack()
-
-        self.botao1 = Button(self.fr1)
-        self.botao1['background'] = "#aa8c12"
-        self.botao1['font'] = ('Verdana', '12')
-        self.botao1.bind("<Button-1>", self.muda_cor)
-        self.botao1.pack()
+        self.fr1['background'] = "#316b2d"
+        self.fr1['height'] = 140
+        self.fr1.pack(side="top", fill = "x")
         
-    
-    def muda_cor(self, toplevel, event):
-        toplevel.configure(bg=random(1,15000))
+        #BOTÃO DE LOGIN
+        self.botaoLogin = Button(self.fr1)
+        self.botaoLogin['background'] = "#546353"
+        self.botaoLogin['foreground'] = "#546353"
+        self.botaoLogin['font'] = ('Verdana', '12')
+        self.botaoLogin['text'] = "Login"
+        self.botaoLogin.bind("<Button-1>", self.muda_cor)
+        self.botaoLogin.pack(side="right")
+        
+        #BARRA DE PESQUISA
+        self.barraPesquisa = Entry(self.fr1)
+        self.barraPesquisa['background'] = "#ffffff"
+        self.barraPesquisa['text'] = "Oii To aqui"
+        self.barraPesquisa.pack(side='top', anchor='center')
+
+    def muda_cor(self, event):
+        self.botaoLogin['background'] = 'yellow'
 
 
 root = tkk.Tk()
