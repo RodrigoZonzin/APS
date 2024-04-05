7e27ca400a92723a16c3da2735d6b4b041404695
RodrigoZonzin <rodrigojzonzin@gmail.com>
2024-04-05 17:10:58 -0300

    commit 2024-04-05 by zonzin

--- view/JanelaPrincipal.py
+++ view/JanelaPrincipal.py
@@ -1,5 +1,6 @@
 import tkinter as tkk
 from tkinter import *
+from PIL import ImageTk, Image
 import random
 
 class Janela:
@@ -24,13 +25,9 @@ class Janela:
         self.botaoLogin['font'] = ('Verdana', '12')
         self.botaoLogin['text'] = "Login"
         self.botaoLogin.bind("<Button-1>", self.muda_cor)
-        self.botaoLogin.pack(side="right")
+        #self.botaoLogin.grid(column=2, row=0)
+        self.botaoLogin.pack(side= 'right')
         
-        #BARRA DE PESQUISA
-        self.barraPesquisa = Entry(self.fr1)
-        self.barraPesquisa['background'] = "#ffffff"
-        self.barraPesquisa['text'] = "Oii To aqui"
-        self.barraPesquisa.pack(side='right')
 
         #BOTÃO VOLTAR
         self.botaoVoltar = Button(self.fr1)
@@ -39,13 +36,71 @@ class Janela:
         self.botaoVoltar['text'] = "Voltar"
         #self.botaoVoltar.bind("<Button-1>", self.voltar)
         self.botaoVoltar.pack(side = 'left', anchor='center')
+        #self.botaoVoltar.grid(column=0, row=0)
 
+
+        #BARRA DE PESQUISA
+        self.barraPesquisa = Entry(self.fr1)
+        self.barraPesquisa['background'] = "#ffffff"
+        self.barraPesquisa['width'] = 40
+        self.barraPesquisa['fg'] = 'black'
+        self.barraPesquisa['justify'] = 'center'
+        self.barraPesquisa['textvariable'] = "Pesquise uma rota ou local turistico"
+        self.barraPesquisa.pack(side='top', pady=10)
+        #self.barraPesquisa.grid(column=1, row=0)
+
+
+
+
+        #CONTAINER QUE CONTERÁ A LISTAGEM DE ATRAÇÕES TURISTICAS
+        self.frameListagemAtracoes = Frame(toplevel)
+        self.frameListagemAtracoes['background'] = "#44404a"
+        #self.frameListagemAtracoes['height'] = 500
+        self.frameListagemAtracoes['width'] = 550
+        self.frameListagemAtracoes.pack(side = 'top', fill = "y", pady=30)
+
+        # ATRAÇÃO 1
+        self.Atracao1 = Frame(self.frameListagemAtracoes)
+        self.Atracao1['background'] = "gray"
+        self.Atracao1['height'] = 100
+        self.Atracao1.pack(side='top', fill='y')
+
+        self.ReferenciaImgAtracao = ImageTk.PhotoImage(Image.open("APS/view/imgs/tiradentes.jpg").resize((200, 100)))
+        self.imgAtracao1 = Label(self.Atracao1, image=self.ReferenciaImgAtracao, width = 200, height=100)
+        self.imgAtracao1.image = self.ReferenciaImgAtracao
+        self.imgAtracao1.grid(column=0, row = 0)
+        
+        self.txtAtracao1 = Label(self.Atracao1, wraplength=200)
+        self.txtAtracao1['width'] = 50
+        self.txtAtracao1['text'] = "A rota turistica de Tiradentes é lembrada pela itensa aproximação histórica entre o viajante e a paisagem...."
+        self.txtAtracao1['bg'] = "yellow"
+        self.txtAtracao1.grid(column=1, row = 0)
+
+        #ATRACAO TURISTICA 2
+        self.Atracao2 = Frame(self.frameListagemAtracoes)
+        self.Atracao2['background'] = "gray"
+        self.Atracao2['height'] = 100
+        self.Atracao2.pack(side='top', fill='y')
+
+        self.ReferenciaImgAtracao2 = ImageTk.PhotoImage(Image.open("APS/view/imgs/saopaulo.jpg").resize((200, 100)))
+        self.imgAtracao2 = Label(self.Atracao2, image=self.ReferenciaImgAtracao2, width = 200, height=100)
+        self.imgAtracao2.image = self.ReferenciaImgAtracao
+        self.imgAtracao2.grid(column=0, row = 0)
 
         
+        self.txtAtracao2 = Label(self.Atracao2, wraplength=200)
+        self.txtAtracao2['width'] = 50
+        self.txtAtracao2['text'] = "A rota turistica de São Paulo permite uma imersão no ambiente cosmopolita da sociedade pós industrial nazicapitalista"
+        self.txtAtracao2['bg'] = "yellow"
+        self.txtAtracao2.grid(column=1, row = 0)
+
+
 
 
     def muda_cor(self, event):
         self.botaoLogin['background'] = 'yellow'
+
+
 
 
 root = tkk.Tk()
